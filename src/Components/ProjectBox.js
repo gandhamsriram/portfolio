2c61967d9a706556afc16dd3fcbd86687c2593e4
gandhamsriram <gandhamsriramguptha@gmail.com>
2026-07-03 09:48:37 +0530

    Add files

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -7,10 +7,10 @@ const ProjectBox = ({ projectPhoto, projectName }) => {
     "Hostel Booking System Github": "https://github.com/gandhamsriram/hbs-",
 
     "AI-Powered Cloud Chatbot Using AWS and Hugging Face Desc": "Built an AI chatbot using Amazon Lex, AWS Lambda, and Hugging Face. Implemented a serverless cloud architecture using AWS services, and developed a responsive web interface for real-time user interaction.",
-    "AI-Powered Cloud Chatbot Using AWS and Hugging Face Github": "https://github.com/gandhamsriram/cloud-project",
+    "AI-Powered Cloud Chatbot Using AWS and Hugging Face Github": "https://github.com/gandhamsriram/cloud-p",
 
     "Loan Prediction System Using Machine Learning Desc": "A machine learning project implementing SVM, Decision Tree, KNN, and Random Forest models to predict loan approvals. Performs model evaluations based on accuracy, precision, recall, and F1-score to identify the highest performing model.",
-    "Loan Prediction System Using Machine Learning Github": "https://github.com/gandhamsriram/ml-project",
+    "Loan Prediction System Using Machine Learning Github": "https://github.com/gandhamsriram/mlproject",
 
     "Expense Tracker Web Application Desc": "A responsive expense tracking web application built using React.js. Implements JSON-based local storage for persistent data management and uses a modular, component-based UI architecture.",
     "Expense Tracker Web Application Github": "https://github.com/gandhamsriram/fsd1",
